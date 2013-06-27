8728f267d191324d894329fbcb136ae42d37d0b1
Jain Waldrip <jkwaldri@indiana.edu>
2013-06-27 14:28:12 -0400

    Fix session handling.

--- lib/smoketest/session.rb
+++ lib/smoketest/session.rb
@@ -16,6 +16,8 @@ module OLE_QA::Smoketest
   # The main Smoketest Session Handling class.
   class Session
 
+    include OLE_QA::Tools::Asserts
+
     # The options with which the OLE Smoketest session was invoked.
     attr_reader :opts
 
@@ -37,15 +39,46 @@ module OLE_QA::Smoketest
     # The Selenium::WebDriver::Driver session.
     attr_accessor :browser
 
+    # The OLE_QA::Framework session.
+    attr_accessor :framework
+
+    # The start date for this session. (YYYY-MM-DD-HHMM)
+    attr_reader :date
+
+    # The timeout (in seconds) used by asserts and wait_until_presents.
+    attr_reader :explicit_wait
+
+    # The default timeout for Watir-Webdriver.
+    attr_reader :implicit_wait
+
     # @param [Hash] opts The options to pass to the Smoketest session.
-    # @param [Hash] opts[:basic] The basic OLE Smoketest session configuration options.
-    # @param [Hash] opts[:caps] Configuration options for Selenium::Webdriver browser capabilities.
-    # @param [Hash] opts[:sauce] Configuration options for SauceLabs.
+    # @param opts [Symbol] :browser The browser to use when starting the Watir-Webdriver session.
+    # @param opts [String] :base_url The base URL for the OLE installation to be tested.
+    # @param opts [String] :ls_url The Library System/Rice 2 URL for the OLE installation to be tested.
+    # @param opts [Integer] :explicit_wait The wait period (in seconds) to be used for spin asserts.
+    # @param opts [Integer] :implicit_wait The wait period (in seconds) to be used by the Watir::Browser session.
+    #   (This should almost always be 0.)
+    # @param opts [String] :name The name to use for this session.
+    #   (Used by :caps and logging.)
+    # @param opts [Boolean] :headless? Use headless gem to create an XVFB session?
+    #   (Unnecessary if using a SauceLabs session.)
+    # @param opts [Boolean] :capabilities? Use Selenium::WebDriver::Remote::Capabilities?
+    #   (Required if using a SauceLabs session.)
+    # @param opts [Hash] :caps Desired capabilities settings for Selenium::WebDriver::Remote::Capabilities.
+    # @param caps [Fixnum] :version Version number to use.
+    # @param caps [String] :platform The platform string to use.
+    #   (e.g. "Windows 7")
+    # @param opts [Boolean] :sauce? Use a SauceLabs session for testing?
+    # @param opts [String] :sauce_username The username to use for the SauceLabs session.
+    # @param opts [String] :sauce_api_key The private API key to use for the SauceLabs session.
     def initialize(opts = {})
 
       # Parse default options in lib/config/options.yml and merge with specified options.
       default_opts = YAML.load(File.open('lib/config/options.yml'))
-      @opts = opts.merge(default_opts)
+      @opts = default_opts.merge(opts)
+
+      # Set date in format YYYY-MM-DD-HHMM
+      @date = Time.new.strftime("%Y-%m-%d-%H%M")
 
       # Export options to instance variables.
       @base_url = @opts[:base_url]
@@ -53,18 +86,14 @@ module OLE_QA::Smoketest
       @ls_url = @opts[:ls_url]
       @explicit_wait = @opts[:explicit_wait]
       @implicit_wait = @opts[:implicit_wait]
+      @name = @opts[:name] + " (v#{VERSION}) " + @date
 
       # If we're using Selenium::WebDriver::Capabilities, load the appropriate capabilities.
       if @opts[:capabilities?]
-        # Set @caps.name to name specified in options + current date & time.
-        @opts[:caps][:name] = @opts[:caps][:name] + " (v#{VERSION}) - " + Chronic.parse('now').to_s
-        @name = @opts[:caps][:name]
-        @caps = Selenium::WebDriver::Remote::Capabilities.send(@opts[:caps])
-        @caps.version = @opts[:caps][:version]
-        @caps.platform = @opts[:caps][:platform]
+        @caps = Selenium::WebDriver::Remote::Capabilities.send(@opts[:browser])
+        @caps.version = @opts[:version]
+        @caps.platform = @opts[:platform]
         @caps[:name] = @name
-      else
-        @name = "OLE Smoketest (v#{VERSION}) - #{Chronic.parse('now').to_s}"
       end
 
       # If we're running on SauceLabs, connect through the appropriate URL.
@@ -73,9 +102,9 @@ module OLE_QA::Smoketest
       if @opts[:sauce?]
         # Capabilities must be specified if SauceLabs will be used.
         raise StandardError, "Capabilities are required for a SauceLabs session." unless @opts.has_key?(:caps)
-        @browser = Selenium::WebDriver::Driver.for(
+        @browser = Watir::Browser.new(
             :remote,
-            :url => "http://#{@opts[:sauce][:username]}:#{@opts[:sauce][:api_key]}@ondemand.saucelabs.com:80/wd/hub",
+            :url => "http://#{@opts[:sauce_username]}:#{@opts[:sauce_api_key]}@ondemand.saucelabs.com:80/wd/hub",
             :desired_capabilities => @caps
         )
       else
@@ -83,19 +112,18 @@ module OLE_QA::Smoketest
           @headless = Headless.new
           @headless.start
         end
-        if @opts[:browser].instance_of?(Selenium::WebDriver::Driver)
-          @browser = @opts[:browser]
-        else
-          @browser = Watir::Browser.new(@opts[:basic][:browser])
-          @browser.driver.manage.timeouts.implicit_wait = @opts[:basic][:implicit_wait]
-        end
+        @browser = Watir::Browser.new(@opts[:browser])
+        @browser.driver.manage.timeouts.implicit_wait = @opts[:implicit_wait]
       end
+
+      # Start OLE_QA::Framework session.
+      @framework = OLE_QA::Framework.new(:browser => @browser)
     end
 
     # Exit the OLE_QA::Smoketest::Session, quitting the browser and destroying the headless session if it exists.
     def quit
       @browser.quit
-      @headless.destroy if @opts[:headless?]
+      @headless.destroy if @opts[:headless?] && !@opts[:sauce?]
     end
   end
 end
